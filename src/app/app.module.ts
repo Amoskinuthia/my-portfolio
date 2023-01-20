c554419bdf078fc7cd62cb221f59c23ff4fbe98b
amos kinuthia <amoskinuthia15@gmail.com>
2023-01-20 09:21:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    FooterComponent,
+    ContactsComponent,
+    ArticlesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
