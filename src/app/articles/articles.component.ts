e60ea9c36cd19b4f4d783ca941b71ef025bbdaea
Amoskinuthia <Amoskinuthia15@gmail.com>
2023-04-06 11:00:45 +0300

    worked on the projects and articles components

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef,HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-articles',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    // Get the button element
+    const mybutton = this.elementRef.nativeElement.querySelector('#myBtn');
+  
+    // Add a scroll event listener to the window
+    this.renderer.listen(window, 'scroll', () => {
+      // If the user has scrolled down 20px, show the button
+      if (window.pageYOffset > 20) {
+        this.renderer.setStyle(mybutton, 'display', 'block');
+      } else {
+        this.renderer.setStyle(mybutton, 'display', 'none');
+      }
+    });
   }
+  animationState = 'hide';
 
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset;
+    const pricingElement = this.elementRef.nativeElement.querySelector('.card');
+    const pricingPosition = pricingElement.getBoundingClientRect().top + window.pageYOffset;
+
+    if (scrollPosition >= pricingPosition - 400) {
+      this.animationState = 'show';
+    }
+    else {
+      this.animationState = 'hide';
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,27 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef,HostListener } from '@angular/core';
 import { trigger, state, style, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
-  animations: [
-    trigger('pricingAnim', [
-      transition('*<=>*', [
-        query('.card', style({ opacity: 0, transform: 'translateX(-40px)' })),
-        query('.card', stagger('300ms', [
-          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
-        ])),
-      ])
-    ])
-  ]
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+ 
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    // Get the button element
+    const mybutton = this.elementRef.nativeElement.querySelector('#myBtn');
+  
+    // Add a scroll event listener to the window
+    this.renderer.listen(window, 'scroll', () => {
+      // If the user has scrolled down 20px, show the button
+      if (window.pageYOffset > 20) {
+        this.renderer.setStyle(mybutton, 'display', 'block');
+      } else {
+        this.renderer.setStyle(mybutton, 'display', 'none');
+      }
+    });
   }
+  animationState = 'hide';
 
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset;
+    const pricingElement = this.elementRef.nativeElement.querySelector('.card');
+    const pricingPosition = pricingElement.getBoundingClientRect().top + window.pageYOffset;
+
+    if (scrollPosition >= pricingPosition - 400) {
+      this.animationState = 'show';
+    }
+    else {
+      this.animationState = 'hide';
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
 }
 
